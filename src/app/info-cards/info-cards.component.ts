f08b1896c29c8ea8fb4bcdbc663cc2b43d564190
Daniel Rincon <danielrincongamboa@gmail.com>
2025-04-28 00:34:08 -0500

    Actualizacion componente cards

--- src/app/info-cards/info-cards.component.ts
+++ src/app/info-cards/info-cards.component.ts
@@ -9,5 +9,26 @@ import { Component } from '@angular/core';
 
 
 export class InfoCardsComponent {
-
+  cards = [
+    {
+      title: 'Oficina Virtual',
+      image: 'assets/cards_images/oficina_Virtual.png',
+      link: 'https://www.ejemplo.com'
+    },
+    {
+      title: 'Mi Pago Amigo',
+      image: 'assets/cards_images/oficina_Virtual.png',
+      link: 'https://www.ejemplo.com'
+    },
+    {
+      title: 'Campus',
+      image: 'assets/cards_images/oficina_Virtual.png',
+      link: 'https://www.ejemplo.com'
+    },
+    {
+      title: 'App Coonadoc',
+      image: 'assets/cards_images/oficina_Virtual.png',
+      link: 'https://www.ejemplo.com'
+    }
+  ];
 }
