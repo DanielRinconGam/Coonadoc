7f56f14af346f355d08ebb1f2e523715f655f237
Daniel Rincon <danielrincongamboa@gmail.com>
2025-05-07 22:08:39 -0500

    Nuevo componente formats

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './about/about.component';
 import { OnlineComponent } from './pages/online/online.component';
+import { FormatsComponent } from './pages/formats/formats.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent }, 
     { path: 'inicio', redirectTo: '', pathMatch: 'full' },
     { path: 'about', component: AboutComponent},
     { path: 'online', component: OnlineComponent },
+    { path: 'services/formats', component: FormatsComponent }
   ];
---
+++ src/app/pages/formats/formats.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formats',
+  imports: [],
+  templateUrl: './formats.component.html',
+  styleUrl: './formats.component.css'
+})
+export class FormatsComponent {
+
+}
