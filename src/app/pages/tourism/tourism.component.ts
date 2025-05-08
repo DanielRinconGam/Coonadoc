e7cb8e260cc770e929cf52be0f0adeecaf217bd7
Ney <ney@Neyders-MacBook-Pro.local>
2025-05-08 16:42:47 -0500

    Cambio en tarjetas de formatos, creditos, politicas y turismo

--- src/app/pages/tourism/tourism.component.ts
+++ src/app/pages/tourism/tourism.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { AboutComponent } from "../../about/about.component";
 
 @Component({
   selector: 'app-tourism',
-  imports: [],
+  imports: [AboutComponent],
   templateUrl: './tourism.component.html',
   styleUrl: './tourism.component.css'
 })
